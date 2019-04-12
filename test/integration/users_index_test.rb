04b96bea4fa46e7101bfb1788f3dfebd18421d09
tsubasa.masaki <tsubasa.masaki@tsubasamasakinoMacBook-puro.local>
2019-04-12 17:26:03 +0900

    Add account activation

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -1,19 +1,44 @@
 require 'test_helper'
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
-  
   def setup
-    @admin = users(:michael)
+    @admin     = users(:michael)
     @non_admin = users(:archer)
   end
 
-  test "index as admin including pagination and delete links" do
+  # test "index as admin including pagination and delete links" do
+  #   log_in_as(@admin)
+  #   get users_path
+  #   assert_template 'users/index'
+  #   assert_select 'div.pagination'
+  #   first_page_of_users = User.paginate(page: 1)
+  #   assert_select 'img', count:30
+  #   assert_select 'a.user-link'
+  #   assert_select 'a',text: "← Previous"
+  #   first_page_of_users.each do |user|
+  #     # assert_select 'a',text: user.name
+  #     # assert_select 'a.user-link', text: user.name
+  #     assert_match "aaa", response.body
+  #     assert_match "/users/", response.body
+  #     # assert_select 'a[href=?]', user_path(user), text: user.name
+  #     unless user == @admin
+  #       assert_select 'a[href=?]', user_path(user), text: 'delete'
+  #     end
+  #   end
+  #   assert_difference 'User.count', -1 do
+  #     delete user_path(@non_admin)
+  #   end
+  # end
+   test "index as admin including pagination and delete links" do
     log_in_as(@admin)
     get users_path
     assert_template 'users/index'
-    assert_select 'div.pagination', count: 2
-    first_page_of_users = User.paginate(page: 1)
-    first_page_of_users.each do |user|
+    assert_select 'div.pagination'
+    assert_select 'img', count:30
+    # first_page_of_users = User.paginate(page: 1)
+    # first_page_of_users.each do |user|
+    users = User.all
+    users do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
         assert_select 'a[href=?]', user_path(user), text: 'delete'
@@ -29,5 +54,4 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_select 'a', text: 'delete', count: 0
   end
-
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
   
   test "invalid signup information" do
     get signup_path
@@ -20,14 +24,27 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'form[action="/signup"]'
   end
 
-  test "acsept user" do
+  test "valid signup information with account activation" do
     get signup_path
-    assert_difference 'User.count' do
+     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name: "Example User",
                                   email: "user@example.com",
                                   password: "password",
                                   password_confirmation: "password"} }
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    # 有効化していない状態でログイン
+    log_in_as(user)
+    assert_not is_logged_in?
+    # 有効化トークンが不正な場合
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+    # トークンは正しいがメールアドレスが無効な場合
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    # 有効化トークンが正しい場合
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect!
     assert_template 'users/show'
     assert_not flash.empty?
